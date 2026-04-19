ed56e97105444537407bac839fe06bfb7677d5b0
ITO HARUTO <haruto.0827@au.com>
2026-04-19 23:47:05 +0900

    fix DailyTotal

--- src/main/java/haruto/DailyTotal.java
+++ src/main/java/haruto/DailyTotal.java
@@ -1,3 +1,16 @@
 package haruto;
 
-public record DailyTotal(String day, int total) {}
+public final clas DailyTotal{
+private final String day;
+private final String total;
+public DailyTotal(String day,int total){
+    this.day=day;
+    this.total=total;
+}
+public String day(){
+    return day;
+}
+public int total(){
+    return total;
+}
+        }
